f804015f40fbea859344c45435f605114f48fd7f
Donovan Jones <donovanljones94@gmail.com>
2019-08-12 14:49:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-    return "HUH? SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
     
